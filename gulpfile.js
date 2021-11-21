fd933e3848563a5089dfe7eaa3b8f3c6f498beb9
Patrick Tonderai Ganhiwa <ptganhiwa8@gmail.com>
2021-11-21 17:23:25 +0200

    gulp file stable and running

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
-// const { watch } = require('browser-sync');
-const { watch, series } = require('gulp'); //src, dest, watch,
+const { watch, series } = require('gulp'); 
 const gulp = require('gulp');
 const browsersync = require('browser-sync').create();
 const cleanCss = require('gulp-clean-css');
-// const imagemin = require('imagemin');
-// const imageminMozjpeg = require('imagemin-mozjpeg');
-
 
 /**
  *  -- TOP LEVEL FUNTIONS --
@@ -16,61 +12,45 @@ const cleanCss = require('gulp-clean-css');
  */
 
 // Logs Message
-gulp.task('message', async function(){
+async function message(){
     return console.log('Gulp is running now...');
-});
-
-// gulp.task('default', async function(){
-//     return console.log('Gulp is running now...');
-// });
+};
 
 // COpy all html files
-gulp.task('copyHtml', async () => {
+async function copyHtml(){
     gulp.src('*.html')
     .pipe(gulp.dest('dist'));
-});
+};
 
 // Task to minify css using the cleanCss package variable
-gulp.task('minify-css', async () => {
+async function minifyCss() {
     gulp.src('src/css/*.css')
     .pipe(cleanCss({compatibility: 'ie8'}))
     .pipe(gulp.dest('./dist/css'));
-});
-
-// Minify images
-// gulp.task('minify-image', async () => {
-
-//     await imagemin(['src/images/*.jpg', './dist/images', {
-//         use: [imageminMozjpeg()]
-//     }]);
-// });
-
-// gulp.src('src/images/*.jpg')
-// .pipe(imagemin())
-// .pipe(gulp.dest('./dist/images'));
-
+};
 
 // Browsersync Task
-gulp.task('syncBrowserServe', async (cb) => {
+async function syncBrowserServe(cb){
     browsersync.init({
         server: {
             baseDir: '.'
         }
     });
     cb();
-});
+};
 
 // BrowsersyncReload - reloads our browser
-gulp.task('browsersyncReload', async (cb) => {
+async function browsersyncReload(cb) {
     browsersync.reload();
     cb();
-});
+};
 
 // Our Watch task keeps an eye on our files detect change
-// gulp.task('', async () => {
-//     watch('*.html', browsersyncReload); // change detected in html files
-//     watch('src/css/**/*.css', series(minify-css, browsersyncReload)); // if any change is detected in css files then reload. if you have js and css then use []
-// });
+async function watchTask(){
+    watch('*.html', browsersyncReload); // change detected in html files
+    watch('src/css/**/*.css', series(minifyCss, browsersyncReload)); // if any change is detected in css files then reload. if you have js and css then use []
+};
 
 // Default Gulp task (My gulp workflow)
-// exports.default = series(message, copyHtml, minify-css, syncBrowserServe, watchTask);
+// exports.build = build;
+exports.default = series(message, copyHtml, minifyCss, syncBrowserServe, watchTask);
